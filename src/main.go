6e50554eef51e5d797f5e8e2641b478c466c5461
Alex Mapley <Alex@Alexs-MBP.home>
2018-06-13 17:01:00 -0400

    adds tree spawning

--- src/main.go
+++ src/main.go
@@ -4,12 +4,16 @@ import (
 	"image"
 	"os"
 	"time"
+	"math"
+	"math/rand"
 
 	_ "image/png"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	//"github.com/faiface/pixel/text"
 	"golang.org/x/image/colornames"
+	//"golang.org/x/image/font/basicfont"
 )
 
 
@@ -24,7 +28,7 @@ func run() {
 
   cfg := pixelgl.WindowConfig{
 		Title:  "Boss Fight!",
-		Bounds: pixel.R(0, 0, 1024, 768),
+		Bounds: pixel.R(0, 0, 1024, 1024),
 		VSync:  true,
 	}
 
@@ -36,14 +40,30 @@ func run() {
 	// Removes pixelation effect on sprite movement
 	win.SetSmooth(true)
 
+
+	// Load in trees
+	treeSheet, err := loadPicture("../images/trees.png")
+	if err != nil {
+		panic(err)
+	}
+	var treesFrames []pixel.Rect
+	for x := treeSheet.Bounds().Min.X; x < treeSheet.Bounds().Max.X; x += 32 {
+		for y := treeSheet.Bounds().Min.Y; y < treeSheet.Bounds().Max.Y; y += 32 {
+			treesFrames = append(treesFrames, pixel.R(x, y, x+32, y+32))
+		}
+	}
+	var (
+		trees    []*pixel.Sprite
+		matrices []pixel.Matrix
+	)
+
+	// Load in main character
 	dolphPic, err := loadPicture("../images/dolph.png")
 	if err != nil {
 		panic(err)
 	}
-
 	dolph := pixel.NewSprite(dolphPic, dolphPic.Bounds())
 
-	win.Clear(colornames.Skyblue)
 
 	// Pixe.IM stands for Pixel "Identity Matrix"
 	mat := pixel.IM
@@ -52,6 +72,14 @@ func run() {
 	dolph.Draw(win, mat)
 
 	
+	// Camera field
+	var (
+		camPos   = pixel.ZV
+		camSpeed = 500.0
+		camZoom      = 1.0
+		camZoomSpeed = 1.2
+	)
+	
 	// Sprite attributes
 	dolphAngle := 0.0
 
@@ -59,14 +87,46 @@ func run() {
 	lastFrameTime := time.Now()
 
 	// Main window loop
-  for !win.Closed() {
+  	for !win.Closed() {
+
+		// Clear screen
+		win.Clear(colornames.Whitesmoke)
 
 		// Global fields
 		deltatTime := time.Since(lastFrameTime).Seconds()
 		lastFrameTime = time.Now()
 
-		// Clear screen
-		win.Clear(colornames.Skyblue)
+		// Plant trees
+		if win.JustPressed(pixelgl.MouseButtonLeft) {
+			tree := pixel.NewSprite(treeSheet, treesFrames[rand.Intn(len(treesFrames))])
+			trees = append(trees, tree)
+			matrices = append(matrices, pixel.IM.Scaled(pixel.ZV, 2).Moved(win.MousePosition()))
+		}
+		for i, tree := range trees {
+			tree.Draw(win, matrices[i])
+		}
+
+		// Moving the character
+		cam := pixel.IM.Scaled(camPos, camZoom).Moved(win.Bounds().Center().Sub(camPos))
+		win.SetMatrix(cam)
+		if win.Pressed(pixelgl.KeyLeft) {
+			camPos.X -= camSpeed * deltatTime
+		}
+		if win.Pressed(pixelgl.KeyRight) {
+			camPos.X += camSpeed * deltatTime
+		}
+		if win.Pressed(pixelgl.KeyDown) {
+			camPos.Y -= camSpeed * deltatTime
+		}
+		if win.Pressed(pixelgl.KeyUp) {
+			camPos.Y += camSpeed * deltatTime
+		}
+		if win.Pressed(pixelgl.KeyUp) {
+			camPos.Y += camSpeed * deltatTime
+		}
+		camZoom *= math.Pow(camZoomSpeed, win.MouseScroll().Y)
+
+		
 
 		// Attribute updates
 		dolphAngle += 1.5 * deltatTime
@@ -75,7 +135,7 @@ func run() {
 
 		// Drawing Dolph
 		mat := pixel.IM
-		mat = mat.Rotated(pixel.ZV, dolphAngle)
+		//mat = mat.Rotated(pixel.ZV, dolphAngle)
 		mat = mat.Moved(win.Bounds().Center())
 		mat = mat.ScaledXY(win.Bounds().Center(), pixel.V(0.25, 0.25))
 		dolph.Draw(win, mat)
